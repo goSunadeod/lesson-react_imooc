05fcede51c24bd1b66ebe9d5420fbe5c5c60efe5
sundae <haiqiang.xu@hand-china.com>
2019-08-24 23:45:06 +0800

    react-redux demo

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,6 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import reducer from './reducer';
-import createSagaMiddleware from 'redux-saga';
-import todoSagas from './sagas';
-// import thunk from 'redux-thunk';
+import { createStore } from 'redux';
+import reducer from './reduce';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-// const enhancer = composeEnhancers(
-//   applyMiddleware(thunk),
-// );
-const sagaMiddleware = createSagaMiddleware()
-const enhancer = composeEnhancers(
-  applyMiddleware(sagaMiddleware)
-);
+const store = createStore(reducer);
 
-const store = createStore(
-  reducer,
-  enhancer
-)
-sagaMiddleware.run(todoSagas)
 export default store;
