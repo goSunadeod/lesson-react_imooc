c80cffbf164a78456e1d0d5c2935f6ea1a15add5
sundae <haiqiang.xu@hand-china.com>
2019-08-27 19:22:52 +0800

    immutable

--- src/common/header/store/reduce.js
+++ src/common/header/store/reduce.js
@@ -1,19 +1,16 @@
-import * as constants from './constants'
+import * as constants from './constants';
+import { fromJS } from 'immutable';
 
-const defaultStat = {
+const defaultStat = fromJS({
     focused: false
-};
+});
 
 export default (state = defaultStat, action) => {
     if(action.type === constants.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        return state.set('focused', true);
     }
     if(action.type === constants.SEARCH_BLUR) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false);
     }
     return state;
 };
