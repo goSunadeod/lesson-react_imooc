dd7d05accd060d21c07e9ea4340bf4c3747a4659
sundae <haiqiang.xu@hand-china.com>
2019-08-29 11:10:51 +0800

    asyncComponent

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {GlobalStyled} from './style';
 import {GlobalIcon} from './statics/iconfont/iconfont';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom'
+import { asyncComponent } from './asyncComponent'
 import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable';
 import Login from './pages/login';
-import Writer from './pages/write';
+// import Writer from './pages/write';
 import store from './store';
+
+const Writer = asyncComponent(() => import("./pages/write"))
 
 class App extends Component {
     render() {
